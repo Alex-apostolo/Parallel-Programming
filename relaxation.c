29e95d123d13f59689a85ad8e466c9d01413b281
Alex-apostolo <alex-apostolo@hotmail.com>
2020-11-20 19:49:25 +0200

    Made it work in parallel for 4x4 matrices

--- relaxation.c
+++ relaxation.c
@@ -2,15 +2,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+// TODO: REMOVE BEFORE UPLOADING
+#include "pthread_barrier.c"
+
+pthread_barrier_t swapncheck;
+pthread_barrier_t texit;
+bool success = false;
 
 typedef struct args {
     double **current;
     double **previous;
+    int dimension;
     int startrow;
     int endrow;
     int startcol;
     int endcol;
     double precision;
+    int index;
 } ARGS;
 
 void initSquare(double ***square, int dimension) {
@@ -40,27 +48,42 @@ void *solve(void *args) {
     // Put the fields of the args struct to seperate variables for easier use
     double **current = targs->current;
     double **previous = targs->previous;
+    int dimension = targs->dimension;
     int startrow = targs->startrow;
     int endrow = targs->endrow;
     int startcol = targs->startcol;
     int endcol = targs->endcol;
     double precision = targs->precision;
+    int index = targs->index;
 
-    for (int i = startrow; i < endrow; i++) {
-        for (int j = startcol; j < endcol; j++) {
-            current[i][j] = (previous[i][j - 1] + previous[i - 1][j] +
-                             previous[i][j + 1] + previous[i + 1][j]) /
-                            4;
+    while (success == false) {
+        for (int i = startrow; i < endrow; i++) {
+            for (int j = startcol; j < endcol; j++) {
+                current[i][j] = (previous[i][j - 1] + previous[i - 1][j] +
+                                 previous[i][j + 1] + previous[i + 1][j]) /
+                                4;
+            }
         }
-    }
-    // Wait for all threads to end computation so that the array current is
-    // full, then check if its below the precision, if yes then exit else swap
-    // and continue the same thread
+        pthread_barrier_wait(&swapncheck);
 
-    // double **temp = previous;
-    //     previous = current;
-    //     current = temp;
-    return NULL;
+        if (index == 0) {
+            success = true;
+            for (int i = 1; i < dimension - 1; i++) {
+                for (int j = 1; j < dimension - 1; j++) {
+                    if (current[i][j] - previous[i][j] < precision)
+                        success = success && true;
+                    else
+                        success = success && false;
+                }
+            }
+        }
+        double **temp = previous;
+        previous = current;
+        current = temp;
+
+        pthread_barrier_wait(&texit);
+    }
+    pthread_exit(NULL);
 }
 
 int relaxation(int dimension, int pthreads, double precision) {
@@ -74,32 +97,36 @@ int relaxation(int dimension, int pthreads, double precision) {
     if (pthreads >= calculations) {
         pthread_t tids[calculations];
         ARGS args[calculations];
+        pthread_barrier_init(&swapncheck, NULL, calculations);
+        pthread_barrier_init(&texit, NULL, calculations);
         int c = 0;
         for (int i = 1; i < dimension - 1; i++) {
             for (int j = 1; j < dimension - 1; j++) {
                 args[c].current = current;
                 args[c].previous = previous;
+                args[c].dimension = dimension;
                 args[c].startrow = i;
                 args[c].endrow = i + 1;
                 args[c].startcol = j;
                 args[c].endcol = j + 1;
                 args[c].precision = precision;
-                pthread_create(&tids[i], NULL, solve, &args[c]);
+                args[c].index = c;
+                pthread_create(&tids[c], NULL, solve, &args[c]);
                 c++;
             }
         }
-        for (int i = 0; i < calculations; i++) {
-            pthread_join(tids[i], NULL);
+        for (c = 0; c < calculations; c++) {
+            pthread_join(tids[c], NULL);
         }
     } else if (pthreads % calculations == 0) {
     }
-    printSquare(current,dimension);
+    printSquare(previous, dimension);
     return 0;
 }
 
 int main() {
     // Prompt User for dimension of their array
     // Call relaxation
-    relaxation(4, 4, 0.000001);
+    relaxation(3, 6, 0.0000001);
     return 0;
 }
