f6c61656bc1fb65f0a1dfe6cbe4054f38c41cff2
Alex-apostolo <alex-apostolo@hotmail.com>
2020-12-30 18:08:38 +0200

    Refactored and reduced code space

--- CW2/bla.c
+++ CW2/bla.c
@@ -1,28 +1,109 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void parallelFunc(double **, double **, int, float, int, int);
 
 int main(int argc, char **argv)
 {
-    // Initialize the MPI environment
-    MPI_Init(&argc, &argv);
-  
-    // Get the number of processes ssociated with the communicator
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    double *current;
+    double *previous;
+    parallelFunc(&previous, &current, 5, 0.01, 1, 0);
+}
 
-    // Get the rank of the calling process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+/* Creates array with size dimension^2 with padded edges */
+void initSquare(double **square, int dimension) {
+    int size = dimension * dimension;
+    *square = calloc(1, size * sizeof(double));
+    int i;
+    // Tracks first row
+    int a;
+    // Tracks last row
+    int b;
+    // Tracks first column
+    int c;
+    // Tracks last column
+    int d;
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    // Fills first row, column and last row, column of array with 1s
+    for (i = 0, a = 0, b = size - dimension, c = 0, d = dimension - 1;
+         i < dimension; i++, a++, b++, c += dimension, d += dimension) {
+        (*square)[a] = 1;
+        (*square)[b] = 1;
+        (*square)[c] = 1;
+        (*square)[d] = 1;
+    }
+}
 
-    printf("Hello world from process %s with rank %d out of %d processors\n", processor_name, world_rank, world_size);
+/* Prints array given the dimension */
+void printSquare(double *square, int dimension) {
+    int i;
+    int j;
+    // k keeps track of last index element
+    int k;
+    for (i = 0, k = 0; i < dimension; i++) {
+        for (j = 0; j < dimension; j++, k++) {
+            printf("%f\t", square[k]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
-    // Finalize: Any resources allocated for MPI can be freed
-    MPI_Finalize();
+/* Parallel function responsible for calculating the subpart of the array
+ * depending on the rank And combining everything into the current array of the
+ * first rank
+ */
+void parallelFunc(double **previous, double **current, int dimension,
+                  float precision, int processors, int rank) {
+    // to find the number of calculations this processor will do
+    // take the size and divide it by the number of processors
+    int calculations = (dimension * dimension) / processors;
+    // calculate which range of values this processor will compute
+    int x0 = rank * calculations;
+    int x1 = (rank + 1) * calculations;
+
+    // If previous is NULL then initialize it
+    if (*previous == NULL) {
+        // Only initialize the values needed for the computation of current 
+        // Create an array padded with 1s on the edges
+        initSquare(previous, dimension);
+    }
+    // If current is NULL then initialize it
+    if (*current == NULL) {
+        *current = malloc((x1 - x0) * sizeof(double));
+    }
+
+    int i;
+    int k;
+    for (i = x0, k = 0; i < x1; i++, k++) {
+        if ((*previous)[i] != 1) {
+            (*current)[k] =
+                ((*previous)[i - 1] + (*previous)[i + 1] +
+                 (*previous)[i - dimension] + (*previous)[i + dimension]) /
+                4;
+        } else if ((*previous)[i] == 1 && (*current)[k] != 1) {
+            (*current)[k] = 1;
+        }
+    }
+
+    printSquare(*current, dimension);
+
+    if (rank != 0) {
+        // Send messages to processor 0
+        // int ierr = MPI_Send();
+    }
+    // Barrier responsible for waiting for all the processors to finish
+    // their execution. It is placed right before the combination of the sub
+    // arrays
+    // int ierr = MPI_Barrier(MPI_COMM_WORLD);
+
+    // When every processor is done with their computations combine into one
+    // array. The first processor receives messages from the rest of the
+    // processors which contain their calculated parts of the array
+    if (rank == 0) {
+        // Receive messages and combine arrays to a single one
+    }
 }
 
 long double sumSquare(double **square, int dimension) {
@@ -37,54 +118,54 @@ long double sumSquare(double **square, int dimension) {
     return sum;
 }
 
-/*This method is called when a thread gets created*/
-void *thread_func(void *args) {
-    // Casts void pointer to ARGS pointer
-    ARGS *targs = args;
-    // Puts the fields of the args struct to seperate variables for easier use
-    int index = targs->index;
-    int dimension = targs->dimension;
-    double precision = targs->precision;
-    struct CoordNode *coordinates_first = targs->coordinates_first;
-    // struct CoordNode *coordinates_last = targs->coordinates_last;
+// /*This method is called when a thread gets created*/
+// void *thread_func(void *args) {
+//     // Casts void pointer to ARGS pointer
+//     ARGS *targs = args;
+//     // Puts the fields of the args struct to seperate variables for easier use
+//     int index = targs->index;
+//     int dimension = targs->dimension;
+//     double precision = targs->precision;
+//     struct CoordNode *coordinates_first = targs->coordinates_first;
+//     // struct CoordNode *coordinates_last = targs->coordinates_last;
 
-    while (success == false) {
-        struct CoordNode *temp = coordinates_first;
-        int i, j;
-        while (temp != NULL) {
-            i = temp->tuple.i;
-            j = temp->tuple.j;
-            // No need to lock here since each thread changes different values
-            // inside the array
-            current[i][j] = (previous[i][j - 1] + previous[i - 1][j] +
-                             previous[i][j + 1] + previous[i + 1][j]) /
-                            4;
-            temp = temp->next;
-        }
+//     while (success == false) {
+//         struct CoordNode *temp = coordinates_first;
+//         int i, j;
+//         while (temp != NULL) {
+//             i = temp->tuple.i;
+//             j = temp->tuple.j;
+//             // No need to lock here since each thread changes different values
+//             // inside the array
+//             current[i][j] = (previous[i][j - 1] + previous[i - 1][j] +
+//                              previous[i][j + 1] + previous[i + 1][j]) /
+//                             4;
+//             temp = temp->next;
+//         }
 
-        pthread_barrier_wait(&barrier);
+//         pthread_barrier_wait(&barrier);
 
-        // No need to lock here either since only the first thread is
-        // responsible for changing the success values
-        if (index == 0) {
-            // Checks if each value of the calculated array is below precision
-            success = true;
-            for (i = 1; i < dimension - 1; i++) {
-                for (j = 1; j < dimension - 1; j++) {
-                    if (current[i][j] - previous[i][j] < precision)
-                        success = success && true;
-                    else
-                        success = success && false;
-                }
-            }
+//         // No need to lock here either since only the first thread is
+//         // responsible for changing the success values
+//         if (index == 0) {
+//             // Checks if each value of the calculated array is below precision
+//             success = true;
+//             for (i = 1; i < dimension - 1; i++) {
+//                 for (j = 1; j < dimension - 1; j++) {
+//                     if (current[i][j] - previous[i][j] < precision)
+//                         success = success && true;
+//                     else
+//                         success = success && false;
+//                 }
+//             }
 
-            // Swap previous and current
-            double **temp1 = previous;
-            previous = current;
-            current = temp1;
-        }
+//             // Swap previous and current
+//             double **temp1 = previous;
+//             previous = current;
+//             current = temp1;
+//         }
 
-        pthread_barrier_wait(&barrier1);
-    }
-    pthread_exit(NULL);
-}
+//         pthread_barrier_wait(&barrier1);
+//     }
+//     pthread_exit(NULL);
+// }
--- CW2/relaxation.c
+++ CW2/relaxation.c
@@ -1,5 +1,5 @@
 #include "pthread_barrier.c"
-// #include <mpi.h>
+#include <mpi.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,24 +10,22 @@ void initSquare(double **square, int dimension) {
     int size = dimension * dimension;
     *square = calloc(1, size * sizeof(double));
     int i;
-    // k keeps track of index to be filled
-    int k;
+    // Tracks first row
+    int a;
+    // Tracks last row
+    int b;
+    // Tracks first column
+    int c;
+    // Tracks last column
+    int d;
 
-    // Fills first row of array with 1s
-    for (i = 0, k = 0; i < dimension; i++, k++) {
-        (*square)[k] = 1;
-    }
-    // Fills last row of array with 1s
-    for (i = 0, k = size - dimension; i < dimension; i++, k++) {
-        (*square)[k] = 1;
-    }
-    // Fills first column of array with 1s
-    for (i = 0, k = 0; i < dimension; i++, k = k + dimension) {
-        (*square)[k] = 1;
-    }
-    // Fills last column of array with 1s
-    for (i = 0, k = dimension - 1; i < dimension; i++, k = k + dimension) {
-        (*square)[k] = 1;
+    // Fills first row, column and last row, column of array with 1s
+    for (i = 0, a = 0, b = size - dimension, c = 0, d = dimension - 1;
+         i < dimension; i++, a++, b++, c += dimension, d += dimension) {
+        (*square)[a] = 1;
+        (*square)[b] = 1;
+        (*square)[c] = 1;
+        (*square)[d] = 1;
     }
 }
 
@@ -46,7 +44,11 @@ void printSquare(double *square, int dimension) {
     printf("\n");
 }
 
-void parallelFunc(double **previous, double **current, int dimension,
+/* Parallel function responsible for calculating the subpart of the array
+ * depending on the rank And combining everything into the current array of the
+ * first rank
+ */
+void solver(double **previous, double **current, int dimension,
                   float precision, int processors, int rank) {
     // to find the number of calculations this processor will do
     // take the size and divide it by the number of processors
@@ -55,49 +57,71 @@ void parallelFunc(double **previous, double **current, int dimension,
     int x0 = rank * calculations;
     int x1 = (rank + 1) * calculations;
 
-    for (int i = x0; i < x1; i++) {
+
+    // If previous is NULL then initialize it
+    if (*previous == NULL) {
+        // Only initialize the values needed for the computation of current 
+        // Create an array padded with 1s on the edges
+        initSquare(previous, dimension);
+    }
+    // If current is NULL then initialize it
+    if (*current == NULL) {
+        *current = malloc((x1 - x0) * sizeof(double));
+    }
+
+    int i;
+    int k;
+    // Iterate through range and apply algorithm to previous
+    // Result is stored in currents k index
+    for (i = x0, k = 0; i < x1; i++, k++) {
         if ((*previous)[i] != 1) {
-            (*current)[i] = ((*previous)[i - 1] + (*previous)[i + 1] +
-                             (*previous)[i - dimension] + (*previous)[i + dimension]) /
-                            4;
+            (*current)[k] =
+                ((*previous)[i - 1] + (*previous)[i + 1] +
+                 (*previous)[i - dimension] + (*previous)[i + dimension]) /
+                4;
+        } else if ((*previous)[i] == 1 && (*current)[k] != 1) {
+            (*current)[k] = 1;
         }
     }
+
+    printSquare(*current, dimension);
+
+    if (rank != 0) {
+        // Send messages to processor 0
+        // int ierr = MPI_Send();
+    }
+    // Barrier responsible for waiting for all the processors to finish
+    // their execution. It is placed right before the combination of the sub
+    // arrays
+    int ierr = MPI_Barrier(MPI_COMM_WORLD);
+
+    // When every processor is done with their computations combine into one
+    // array. The first processor receives messages from the rest of the
+    // processors which contain their calculated parts of the array
+    if (rank == 0) {
+        // Receive messages and combine arrays to a single one
+    } 
 }
 
-/* Solver does 3 things
- (1) Initilization of current and previous arrays
- (2) Initialization of MPI and execution of parallel function
- (3) Prints the resulting array (Optionally)
- */
-int solver(double *array, int dimension, double precision) {
-
-    /* Initilization of current and previous arrays
-     * $ Code below runs sequentially $
-     */
-
-    // Size of array argument
-    int size = dimension * dimension;
-
-    // Current and Previous are initialized
-    double *current;
-    double *previous;
-    if (array == NULL) {
-        // If there is no supplied array then create two arrays padded with 1s
-        // on the edges
-        initSquare(&current, dimension);
-        initSquare(&previous, dimension);
-    } else {
-        // Otherwise create one array for the current which will be filled later
-        initSquare(&current, dimension);
-        previous = array;
+int main(int argc, char *argv[]) {
+    // Basic handling of CLI input
+    if (argc != 3) {
+        fprintf(stderr, "Error: need to pass 2 arguments - the dimension "
+                        "and the precision. In that precise order!\n");
+        return -1;
+    }
+    // Dimension is the number of rows or columns, for example dimension = 5
+    // will make an 2D array of 5 x 5
+    int dimension = atoi(argv[1]);
+    double precision = atof(argv[2]);
+    if (dimension <= 0 || precision <= 0.0) {
+        fprintf(stderr, "Error: make sure you entered correct values for "
+                        "dimension, threads and precision\n");
+        return -1;
     }
 
-    /* Initialization of MPI and execution of parallel function
-     * $ Code below runs in parallel $
-     */
-
     // Initialization of the MPI environment
-    MPI_Init(NULL, NULL);
+    int ierr = MPI_Init(NULL, NULL);
 
     // Get number of processors associated with the communicator
     int mpi_size;
@@ -107,39 +131,7 @@ int solver(double *array, int dimension, double precision) {
     int mpi_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-    // Run parallel function here
-    parallelFunc(&previous, &current, dimension, precision, mpi_size,
-    mpi_rank);
 
-    // Finalizes MPI and after this resources can be freed
-    MPI_Finalize();
-
-    /* Prints the resulting array
-     * $ Code below runs sequentially $
-     */
-
-    printSquare(previous, dimension);
-
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    // Basic handling of CLI input
-    if (argc != 4) {
-        fprintf(stderr,
-                "Error: need to pass 3 arguments - the dimension, the number "
-                "\nof threads and the precision. In that precise order!\n");
-        return -1;
-    }
-    // Dimension is the number of rows or columns, for example dimension = 5
-    // will make an array of 5 x 5
-    int dimension = atoi(argv[1]);
-    double precision = atof(argv[2]);
-    if (dimension <= 0 || precision <= 0.0) {
-        fprintf(stderr, "Error: make sure you entered correct values for "
-                        "dimension, threads and precision\n");
-        return -1;
-    }
     // if (pthreads != 1) {
     //    printf("%d,%d,%f,", dimension, pthreads, precision);
     //}
@@ -149,7 +141,11 @@ int main(int argc, char *argv[]) {
 
     // clock_gettime(CLOCK_MONOTONIC, &start);
 
-    solver(NULL, dimension, precision);
+    // Runs solver, calculates current array until the
+    // precision is met
+    double *previous = NULL;
+    double *current = NULL;
+    solver(&previous, &current, dimension, precision, mpi_size, mpi_rank);
 
     // clock_gettime(CLOCK_MONOTONIC, &finish);
 
@@ -162,5 +158,9 @@ int main(int argc, char *argv[]) {
     // } else {
     //     printf("%Lf\n", sumSquare(previous,dimension));
     // }
+
+     // Finalizes MPI and after this resources can be freed
+    MPI_Finalize();
+
     return 0;
 }
