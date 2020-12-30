ded18abea926d2b662ad4fb469c3d0d21f19dd93
Alex-apostolo <alex-apostolo@hotmail.com>
2020-12-30 22:43:31 +0200

    Fixed initilization and printing

--- CW2/relaxation.c
+++ CW2/relaxation.c
@@ -1,109 +1,69 @@
-#include "pthread_barrier.c"
 #include <mpi.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/* Creates array with size dimension^2 with padded edges */
-void initSquare(double **square, int dimension) {
+/* Initializes sub_array for the ranges x0 to x1 of the actual array */
+void init_sub_array(double **sub_array, int dimension, int x0, int x1) {
+    // Total size of array
     int size = dimension * dimension;
-    *square = calloc(1, size * sizeof(double));
-    int i;
-    // Tracks first row
-    int a;
-    // Tracks last row
-    int b;
-    // Tracks first column
-    int c;
-    // Tracks last column
-    int d;
+    // Initialize sub_array with zeroes
+    *sub_array = calloc(1, (x1 - x0) * sizeof(double));
 
-    // Fills first row, column and last row, column of array with 1s
-    for (i = 0, a = 0, b = size - dimension, c = 0, d = dimension - 1;
-         i < dimension; i++, a++, b++, c += dimension, d += dimension) {
-        (*square)[a] = 1;
-        (*square)[b] = 1;
-        (*square)[c] = 1;
-        (*square)[d] = 1;
+    // Fill sub_array
+    int i;
+    int k;
+    for (i = x0, k = 0; i < x1; i++, k++) {
+        // If first row
+        if((i >= 0) && (i < dimension))
+            (*sub_array)[k] = 1;
+        // If last row
+        if((i >= (size - dimension)) && (i < size))
+            (*sub_array)[k] = 1;
+        // If first column
+        if((i % dimension) == 0)
+            (*sub_array)[k] = 1;
+        // // If last column
+        if((i % dimension) == (dimension - 1))
+            (*sub_array)[k] = 1;
     }
 }
 
 /* Prints array given the dimension */
-void printSquare(double *square, int dimension) {
-    int i;
-    int j;
-    // k keeps track of last index element
-    int k;
-    for (i = 0, k = 0; i < dimension; i++) {
-        for (j = 0; j < dimension; j++, k++) {
-            printf("%f\t", square[k]);
-        }
-        printf("\n");
+void print_array(double *array, int dimension, int x0, int x1) {
+    for(int i = 1; i <= (x1-x0); i++) {
+        printf("%f\t", array[i-1]);
+        if((i % dimension) == 0)
+            printf("\n");
     }
-    printf("\n");
 }
 
-/* Parallel function responsible for calculating the subpart of the array
- * depending on the rank And combining everything into the current array of the
- * first rank
- */
-void solver(double **previous, double **current, int dimension,
-                  float precision, int processors, int rank) {
+void solver(double **previous, double **current, int dimension, double precision,
+            int processors, int rank) {
     // to find the number of calculations this processor will do
     // take the size and divide it by the number of processors
     int calculations = (dimension * dimension) / processors;
     // calculate which range of values this processor will compute
-    int x0 = rank * calculations;
-    int x1 = (rank + 1) * calculations;
-
-
-    // If previous is NULL then initialize it
-    if (*previous == NULL) {
-        // Only initialize the values needed for the computation of current 
-        // Create an array padded with 1s on the edges
-        initSquare(previous, dimension);
+    int x0 = rank * dimension;
+    int x1 = x0 + (3 * dimension);
+    printf("x0: %d, x1: %d\n", x0, x1);
+    //init_sub_array(previous, dimension, 4, 16);
+    // Initialize previous and current 
+    if(*previous == NULL) {
+        init_sub_array(previous, dimension, x0, x1);
     }
-    // If current is NULL then initialize it
-    if (*current == NULL) {
-        *current = malloc((x1 - x0) * sizeof(double));
+    if(*current == NULL) {
+        *current = malloc((x1 - x0)*sizeof(double));
     }
-
-    int i;
-    int k;
-    // Iterate through range and apply algorithm to previous
-    // Result is stored in currents k index
-    for (i = x0, k = 0; i < x1; i++, k++) {
-        if ((*previous)[i] != 1) {
-            (*current)[k] =
-                ((*previous)[i - 1] + (*previous)[i + 1] +
-                 (*previous)[i - dimension] + (*previous)[i + dimension]) /
-                4;
-        } else if ((*previous)[i] == 1 && (*current)[k] != 1) {
-            (*current)[k] = 1;
-        }
-    }
-
-    printSquare(*current, dimension);
-
-    if (rank != 0) {
-        // Send messages to processor 0
-        // int ierr = MPI_Send();
-    }
-    // Barrier responsible for waiting for all the processors to finish
-    // their execution. It is placed right before the combination of the sub
-    // arrays
-    int ierr = MPI_Barrier(MPI_COMM_WORLD);
-
-    // When every processor is done with their computations combine into one
-    // array. The first processor receives messages from the rest of the
-    // processors which contain their calculated parts of the array
-    if (rank == 0) {
-        // Receive messages and combine arrays to a single one
-    } 
+    print_array(*previous, dimension, x0, x1);
 }
 
 int main(int argc, char *argv[]) {
+    // // For debugging
+    // argc = 3;
+    // argv[1] = argv[4];
+    // argv[2] = argv[5];
     // Basic handling of CLI input
     if (argc != 3) {
         fprintf(stderr, "Error: need to pass 2 arguments - the dimension "
@@ -130,7 +90,6 @@ int main(int argc, char *argv[]) {
     // Get rank of the calling processor
     int mpi_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-
 
     // if (pthreads != 1) {
     //    printf("%d,%d,%f,", dimension, pthreads, precision);
@@ -159,7 +118,7 @@ int main(int argc, char *argv[]) {
     //     printf("%Lf\n", sumSquare(previous,dimension));
     // }
 
-     // Finalizes MPI and after this resources can be freed
+    // Finalizes MPI and after this resources can be freed
     MPI_Finalize();
 
     return 0;
